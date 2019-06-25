feb6d3be0bc3efa188e12f2c60245c01d56dfca5
juliabock <51519847+juliabock@users.noreply.github.com>
2019-06-25 13:58:12 -0300

    Create problem_4.py
    Recién comenzando a pensarlo. No se si conviene buscar palíndromos y después verificar que sean múltiplos de dos números de 3 cifras o al revez.

---
+++ problem_4.py
@@ -0,0 +1,9 @@
+"""
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+""" 
+
+largest_number = str(999*999)
+if len(largest_number) % 2 == 0:
+  
