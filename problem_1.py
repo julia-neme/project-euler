80b0a52e3696fe49686ae80cdbd8cef60dd0a098
juliabock <51519847+juliabock@users.noreply.github.com>
2019-06-25 13:28:26 -0300

    Update problem_1.py
    Dejo la versión que tarda menos tiempo

--- problem_1.py
+++ problem_1.py
@@ -1,28 +1,7 @@
 """
-El ejercicio pide calcular la suma de todos los números múltiplos de 3 o 5 menores a 1000.
-"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-def es_multiplo(nro):
-  x = False
-  for i in [3, 5]:
-    if nro % i == 0:
-      x = True 
-      break
-  return x
-
-multiplos = []
-for i in range(1, 1000):
-  x = es_multiplo(i)
-  if x:
-    multiplos.append(i)
-
-import numpy as np
-
-suma = np.sum(np.array(multiplos))
-print("La suma de todos los números múltiplos de 3 o 5 menores a 1000 es {0}".format(suma))
-
-"""
-Pruebo un enfoque más eficiente
+Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 def serie_multiplos(N, mult):
