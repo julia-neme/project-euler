4f752a7965d22f95a52bab1cc86efc45c0e5a328
juliabock <51519847+juliabock@users.noreply.github.com>
2019-07-04 10:21:27 -0300

    Update problem_5.py
    Funciona rápido!

--- problem_5.py
+++ problem_5.py
@@ -4,21 +4,52 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
-def get_largest_mult(serie):
-  serie_ord = list(set(serie))
-  mult = []
-  mult.append(serie_ord[-1])
-  i = -2
-  while abs(i) < len(serie_ord):
-    is_mult = True
-    for m in mult:
-      if m % serie_ord[i] == 0:
-        is_mult = False
+def is_prime(N):
+  i = 2
+  ans = True
+  while i*i <= N:
+    if N % i == 0:
+      ans = False
+      break
+    else:
+      i += 1
+  return ans
 
-    if is_mult:
-      mult.append(serie_ord[i])
-    i -= 1
-  return mult
+def get_largest_exp(N_max, N):
+  import numpy as np
+  max_exp = int(np.log(N_max)/np.log(N))
+  return max_exp
+
+def eratosthenes_sieve(N_max):
+  import numpy as np
+  serie = np.arange(1, N_max + 1, 1)
+  sieve = [True] * len(serie)
+  i = 1
+  while i < len(serie):
+    if sieve[i]:
+      if is_prime(serie[i]):
+        max_exp = get_largest_exp(np.max(serie), serie[i])
+        j = 2
+        while j <= max_exp:
+          sieve[serie[i]**j - 1] = False
+          j += 1
+      else:
+        sieve[i] = False
+    i += 1
+  return serie[1:][sieve[1:]]
+
+primos = eratosthenes_sieve(10)
+exp = []
+for i in primos:
+  exp.append(get_largest_exp(10, i))
+primos_elevados = primos**exp
+ans = 1
+for x in primos_elevados:
+  ans = ans*x
+
+print("El menor nro. positivo multiplo de todos los nros. del 1 al 20 es {0}".format(ans))
+      
+  
   
   
        
