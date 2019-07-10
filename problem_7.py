9207d36abcad927fa56f4369a5c148de9455721e
juliabock <51519847+juliabock@users.noreply.github.com>
2019-07-10 15:29:01 -0300

    Update problem_7.py
    Resuelto

--- problem_7.py
+++ problem_7.py
@@ -19,9 +19,9 @@ def get_largest_exp(N_max, N):
   max_exp = int(np.log(N_max)/np.log(N))
   return max_exp
 
-def eratosthenes_sieve(N_max):
+def eratosthenes_sieve(N_min, N_max):
   import numpy as np
-  serie = np.arange(1, N_max + 1, 1)
+  serie = np.arange(N_min, N_max + 1, 1)
   sieve = [True] * len(serie)
   i = 1
   while i < len(serie):
@@ -36,3 +36,12 @@ def eratosthenes_sieve(N_max):
         sieve[i] = False
     i += 1
   return serie[1:][sieve[1:]]
+
+import numpy as np
+N = 10001
+primos = eratosthenes_sieve(1, N)
+while len(primos) < 10001:
+  primos = np.append(primos, eratosthenes_sieve(N, N + 5000))
+  N += 5000
+
+print("The 10001st prime number is {0}".format(primos[10000]))
